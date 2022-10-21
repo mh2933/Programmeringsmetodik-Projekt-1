08600272eb0dc76896cd0e0ec5cbec72eaee7426
mh2933 <42110442+mh2933@users.noreply.github.com>
2022-10-21 15:27:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,21 +5,23 @@ int main(void)
 {
 	setup();
 	
+	/* en kontinuerlig while loop */
     while(1)
 	{
-		if(BUTTON1_IS_PRESSED && !BUTTON2_IS_PRESSED)
+		
+		if(BUTTON1_IS_PRESSED && !BUTTON2_IS_PRESSED) // om bara BUTTON1 är nedtryckt så körs leds_blink_sequence
 		{
-			leds_blink_sequence(100);
+			leds_blink_sequence(1);
 		}
-		else if (BUTTON2_IS_PRESSED && !BUTTON1_IS_PRESSED)
+		else if (BUTTON2_IS_PRESSED && !BUTTON1_IS_PRESSED) // om bara BUTTON2 är nedtryckt så körs leds_blink_sequence_reverse
 		{
-			leds_blink_sequence_reverse(100);
+			leds_blink_sequence_reverse(1);
 		}
-		else if (BUTTON1_IS_PRESSED && BUTTON2_IS_PRESSED)
+		else if (BUTTON1_IS_PRESSED && BUTTON2_IS_PRESSED) // om både BUTTON1 och BUTTON2 är nedtryckt så körs leds_on
 		{
 			leds_on();
 		}
-		else 
+		else // annars körs leds_off
 		{
 			leds_off();
 		}
